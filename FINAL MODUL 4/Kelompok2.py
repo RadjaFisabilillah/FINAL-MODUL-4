ee86bb505affb322cca5cd8127e874c7b7cd72c9
Damairf <damairaya33@gmail.com>
2024-03-29 14:25:25 +0700

    Raya

--- FINAL MODUL 4/Kelompok2.py
+++ FINAL MODUL 4/Kelompok2.py
@@ -1,5 +1,16 @@
+print('KELOMPOK 2')
+print('Damai Raya Fakhruddin (21120123130096)')
 
+def tampilkan_motor(motor_tersedia):
+    """Fungsi untuk menampilkan daftar motor"""
+    list_motor_dengan_Nomor = [f"{i+1}. {motor}" for i, motor in enumerate(motor_tersedia)]
+    for item in list_motor_dengan_Nomor:
+        print(item)
 
+def tambah_motor(motor_tersedia, motor_baru):
+    """Method untuk menambahkan motor baru"""
+    motor_tersedia.append(motor_baru)
+    tampilkan_motor(motor_tersedia)
 
 
 
