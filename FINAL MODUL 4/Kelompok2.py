ff4e0dda6a434b3a4c25cf009d477e5a1d34f79a
Damairf <damairaya33@gmail.com>
2024-03-30 18:32:21 +0700

    Tes aja

--- FINAL MODUL 4/Kelompok2.py
+++ FINAL MODUL 4/Kelompok2.py
@@ -1,9 +1,9 @@
+print('Tugas Modul 4')
 print('KELOMPOK 2')
 print('Damai Raya Fakhruddin (21120123130096)')
 print('Herdika Putra Devara (21120123140112)')
 print('Muhammad Ilham (21120123120003)')
 print('Radja Fisabilillah (21120123130102)')
-
 
 
 class MotorManager:
