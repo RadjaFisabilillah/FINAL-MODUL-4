d9dbbe4649d531a867121117dec6af672e938927
GlynnLeith <muhilham5498@gmail.com>
2024-03-30 23:47:52 +0700

    yeet

--- FINAL MODUL 4/Kelompok2.py
+++ FINAL MODUL 4/Kelompok2.py
@@ -2,8 +2,8 @@ print('Tugas Modul 4')
 print('KELOMPOK 2')
 print('Damai Raya Fakhruddin (21120123130096)')
 print('Herdika Putra Devara (21120123140112)')
-print('Muhammad Ilham (21120123120003)')
 print('Radja Fisabilillah (21120123130102)')
+print('Muhammad Ilham (21120123120003)')
 
 
 class MotorManager:
